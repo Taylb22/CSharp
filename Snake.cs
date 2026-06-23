be8e584827c9ff6f745b583dd035cd61c24418f7
Henrique <hsantos124.br@gmail.com>
2026-06-23 16:51:22 -0300

    help

--- Snake.cs
+++ Snake.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 const int ROWS = 20;
 const int COLUMNS = 20;
@@ -40,15 +42,34 @@ void DisplayTable () {
 Console.ForegroundColor = ConsoleColor.White;
 Console.CursorVisible = false;
 
+var StopWatch = Stopwatch.StartNew();
+double LastTime = StopWatch.Elapsed.TotalSeconds;
+double Accumulator = 0;
+
+const double TICKRATE = 0.25;
+
+//First Apple
 Random Generator = new Random();
 int apple = Generator.Next(0, ROWS*COLUMNS);
 Table[apple] = 2;
+
+//Start Snake
+List<int> Snake = [7];
+Table[7] = 1;
+
+int direction = COLUMNS;
 
 bool IsGameOver = false;
 while (!IsGameOver) {
 	Console.SetCursorPosition(0, 0);
 	Console.WriteLine("\n\nPress Q to Exit");
 	DisplayTable();
+
+	double CurrentTime = StopWatch.Elapsed.TotalSeconds;
+	double DeltaTime = CurrentTime - LastTime;
+	LastTime = CurrentTime;
+
+	Accumulator += DeltaTime;
 	
 	if (Console.KeyAvailable) {
 		var KeyInfo = Console.ReadKey(intercept: true);
@@ -58,16 +79,37 @@ while (!IsGameOver) {
 				IsGameOver = true;
 				break;
 			case ConsoleKey.W:
+				direction = -COLUMNS;
 				break;
 			case ConsoleKey.S:
+				direction = COLUMNS;
 				break;
 			case ConsoleKey.D:
+				direction = 1;
 				break;
 			case ConsoleKey.A:
+				direction = -1;
 				break;
 		}
 	}
 
-	Thread.Sleep(50);
+	if (Table[Snake[Snake[0] + direction] == 2) {
+		Snake.add(Snake[Snake.Count-1]);
+		int last = Snake[0];
+		for (int i = Snake.Count-2; i > 0; i--) {
+			Snake[i] = last;
+			last = Snake[i];
+		}
+	}
+
+	while (Accumulator >= TICKRATE) {
+		Accumulator -= TICKRATE;
+		for (int i = 0; i < Snake.Count-1 ; i++) {
+			Table[Snake[i]] = 0;
+			Snake[i] += direction;
+
+			Table[Snake[i]] = 1;
+		}
+	}
 }
 Console.CursorVisible = true;
